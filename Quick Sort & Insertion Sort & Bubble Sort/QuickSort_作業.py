f151e37b5f69e11d1bdf9a4ec25df9cb5228ae5e
albert0796 <53148084+albert0796@users.noreply.github.com>
2019-11-10 23:15:00 +0800

    Add files via upload

---
+++ Quick Sort & Insertion Sort & Bubble Sort/QuickSort_作業.py
@@ -0,0 +1,20 @@
+def sortQuick(Ary, first=0, last=None):
+    if last==None:
+        last=len(Ary)-1
+    if first < last:
+        pivotIndex=Division(Ary, first, last)
+        sortQuick(Ary, first, pivotIndex-1)
+        sortQuick(Ary, pivotIndex+1, last)
+    return Ary
+
+def Division(Ary, first, last):
+    index=first
+    pivot=Ary[first]
+    for k in range(first+1, last+1):
+        if Ary[k] <= pivot:
+            index+=1
+            Ary[k], Ary[index] = Ary[index], Ary[k]
+    left=Ary[first]
+    Ary[first]=Ary[index]
+    Ary[index]=left
+    return index
