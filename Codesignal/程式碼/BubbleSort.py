8bb80161133e6891cecd7f0b552f02470aef51d7
albert0796 <53148084+albert0796@users.noreply.github.com>
2020-01-07 00:50:38 +0800

    Add files via upload

--- Codesignal/程式碼/BubbleSort.py
+++ Codesignal/程式碼/BubbleSort.py
@@ -1,19 +1,3 @@
-#bubble sort
-data=[7,6,5,3]
-
-for n in range(len(data)-1,0,-1):
-    for i in range(n):
-        if data[i]>data[i+1]:
-            data[i+1],data[i]=data[i],data[i+1]
-######################################################
-data=[3,5,6,7]
-
-for n in range(len(data)-1,0,-1):
-    for i in range(n,0,-1):
-        if data[i]>data[i-1]:
-            data[i-1],data[i]=data[i],data[i-1]
-#####################################################
-
 def SortBubble(data):
     for n in range(len(data)-1,0,-1):
         for i in range(n):
