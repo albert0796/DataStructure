622b40028691d549e2718837828daf5aba82b79d
albert0796 <53148084+albert0796@users.noreply.github.com>
2020-01-07 01:06:27 +0800

    Update 費伯納西數.py

--- Codesignal/程式碼/費伯納西數.py
+++ Codesignal/程式碼/費伯納西數.py
@@ -1,11 +1,3 @@
-def fib_i(n):
-    _1,_2=0,1
-    for i in range(n+1): 
-        _1,_2=_2,_1+_2
-    return _1
-for i in range(10):
-    print(fib_i(i),end="")
-
 def fib_r(n):
     if n==0 or n==1:
         fib=n
